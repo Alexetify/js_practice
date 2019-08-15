c2688d89f596522c70e1d62a75ce62d16a2d3f04
Alexetify <hhmh@rambler.ru>
2019-08-15 23:50:48 +0300

    day_three

--- www/src/index.js
+++ www/src/index.js
@@ -16,8 +16,6 @@ function toggleCheckbox(){
   
   });
 }
-
-
 //end checkbox
 
 //корзина
@@ -37,7 +35,6 @@ function toggleCart(){
     document.body.style.overflow = '';
   });
 }
-
 // end корзина
 
 //добавление, удаление товара в корзину
@@ -46,8 +43,6 @@ function toggleEndCart() {
   const cartWrapper = document.querySelector('.cart-wrapper');
   const cartEmpty = document.getElementById('cart-empty');
   const countGoods = document.querySelector('.counter');
-  
-  
   
   cards.forEach((card) =>{
     const btn = card.querySelector('button');
@@ -91,61 +86,41 @@ function toggleEndCart() {
 }
 //end добавление, удаление товара в корзину
 
-
-
 //Фильтр Акция
 function actionPage() {
   const cards = document.querySelectorAll('.goods .card');
-  const discountCheckbox =document.getElementById('discount-checkbox');
-  //const goods = document.querySelector('.goods');
+  const discountCheckbox = document.getElementById('discount-checkbox');
   const min = document.getElementById('min');
-  const max =document.getElementById('max');
+  const max = document.getElementById('max');
   const search = document.querySelector('.search-wrapper_input');
   const searchBtn = document.querySelector('.search-btn');
 
-  discountCheckbox.addEventListener('click', () => {
-    cards.forEach((card) => {
-      if(discountCheckbox.checked) {
-        if(!card.querySelector('.card-sale')){
-          //card.parentNode.remove();
-          card.parentNode.style.display = 'none';
-        }
+  console.dir(cards[0]);
 
-      } else {
-        //goods.appendChild(card.parentNode);
-
-        card.parentNode.style.display = '';
-
-      }
-
-    });
-
-  });
-
-  min.addEventListener('change', filterPrice);
-  max.addEventListener('change', filterPrice);
-
-  //фильтр
-  function filterPrice() {
-    cards.forEach((card) => {
-      const cardPrice = card.querySelector('.card-price');
-      const price = parseFloat(cardPrice.textContent);
-
-      if ((min.value && price < min.value) || (max.value && price > max.value)){
-        
-        card.parentNode.style.display = 'none';
-        //card.ParentNode.remove();
-      } else {
-
-        card.parentNode.style.display = '';
-        //goods.appendChild(card.parentNode);
-      }
-
-    });
-  }
-
+  discountCheckbox.addEventListener('click', filter);
+  min.addEventListener('change', filter);
+  max.addEventListener('change', filter);
 
   
+
+    function filter() {
+      cards.forEach((card) =>{
+        const cardPrice = card.querySelector('.card-price');
+        const price = parseFloat(cardPrice.textContent);
+        const discount = card.querySelector('.card-sale');
+
+        if ((min.value && price < min.value) || (max.value && price > max.value)) {
+          card.parentNode.style.display = 'none';
+        } else if (discountCheckbox.checked && !discount) {
+          card.parentNode.style.display = 'none';
+        } else {
+          card.parentNode.style.display = '';
+
+        }
+
+      });
+    }
+   
 //поиск
   searchBtn.addEventListener('click', () => {
     const searchText = new RegExp(search.value.trim(), 'i');
@@ -163,6 +138,54 @@ function actionPage() {
 
     });
     search.value = '';
+  });
+
+}
+
+//end Фильтр Акция
+
+// получение данных с сервера
+function getData() {
+  const goodsWrapper = document.querySelector('.goods');
+  return fetch('../db/db.json')
+  .then ((response) => {
+    if(response.ok) {
+      return response.json();
+    } else {
+      throw new Error ('Данные не были получены, ошибка ' + response.status);
+    }
+  })
+  .then ((data) => {
+    return data;
+  })
+  .catch((err) => {
+    console.warn(err);
+    goodsWrapper.innerHTML = '<div style="color: red; font-size: 30px;">Упс, что-то пошло не так(((</div>';
+  });
+
+}
+
+//ввыводим карточки товара
+function renderCards (data){
+  const goodsWrapper = document.querySelector('.goods');
+  data.goods.forEach((good) => {
+    const card = document.createElement('div');
+    card.className = 'col-12 col-md-6 col-lg-4 col-xl-3';
+    card.innerHTML = `
+                <div class="card" data-category="${good.category}">
+                ${good.sale ? '<div class="card-sale">🔥Hot Sale🔥</div>' : ''}
+									<div class="card-img-wrapper">
+										<span class="card-img-top"
+											style="background-image: url('${good.img}')"></span>
+									</div>
+									<div class="card-body justify-content-between">
+										<div class="card-price" style="${good.sale ? 'color: red;' : ''}">${good.price}₽</div>
+										<h5 class="card-title">${good.title}</h5>
+										<button class="btn btn-primary">В корзину</button>
+									</div>
+								</div>			
+    `;
+    goodsWrapper.appendChild(card);
 
 
 
@@ -172,11 +195,63 @@ function actionPage() {
 
 
 }
-//end Фильтр Акция
-toggleCheckbox();
-toggleCart();
-toggleEndCart();
-actionPage();
+//end получение данных с сервера
+
+
+function renderCatalog() {
+  const cards = document.querySelectorAll('.goods .card');
+  const catalogList = document.querySelector('.catalog-list');
+  const catalogWrapper = document.querySelector('.catalog');
+  const catalogBtn = document.querySelector('.catalog-button');
+  const categories = new Set();
+
+  cards.forEach((card) => {
+    categories.add(card.dataset.category);
+  });
+
+  categories.forEach((item) => {
+    const li = document.createElement('li');
+    li.textContent = item;
+    catalogList.appendChild(li);
+
+  });
+
+  catalogBtn.addEventListener('click', (event) => {
+    if (catalogWrapper.style.display) {
+      catalogWrapper.style.display = '';
+    } else {
+    catalogWrapper.style.display = 'block';
+    }
+
+    if (event.target.tagName === 'LI') {
+      cards.forEach ((card) => {
+        if (card.dataset.category === event.target.textContent) {
+          card.parentNode.style.display = '';
+        } else {
+          card.parentNode.style.display = 'none';
+
+        }
+
+      });
+    }
+
+
+  });
+
+
+}
+
+
+getData().then((data) => {
+  renderCards(data);
+  toggleCheckbox();
+  toggleCart();
+  toggleEndCart();
+  actionPage();
+  renderCatalog();
+});
+
+
 
 
 
